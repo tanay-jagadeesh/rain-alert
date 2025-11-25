4a76c1e560bc7de1a5d2af0c5e0d6d94339a6405
Tanay <tanayj2107@gmail.com>
2025-11-25 17:45:07 -0500

    minor link error for api call

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ api_key = "b3b58d3427ba0fac3eff1de660b7c94f"
 MY_LAT = 43.464951
 MY_LONG = -80.523911
 
-response = requests.get(url = f"https.//api.openweathermap.org/data/2.5/forecast?lat={MY_LAT}&lon={MY_LONG}&appid={api_key}")
+response = requests.get(url = f"https://api.openweathermap.org/data/2.5/forecast?lat={MY_LAT}&lon={MY_LONG}&appid={api_key}")
 response.raise_for_status()
 print(response)
